043f2607684c8d1b91573752ae2969f12b95e3d7
hyebeen byun <hyebeen2658@naver.com>
2023-02-20 21:57:11 +0900

    state를 사용 - title 값 설정 변경

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import { ExpenseDate } from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-export function ExpenseItem({ title, amount, date }) {
+export function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button type="button" onClick={clickHandler}>
+        Change Title
+      </button>
     </Card>
   );
 }
