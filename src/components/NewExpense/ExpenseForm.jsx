e5cf8f1ebd91b3236541f37c63ae720677679e4a
hyebeen byun <hyebeen2658@naver.com>
2023-02-21 13:34:31 +0900

    form 제출 -submitHandler

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -3,55 +3,37 @@ import "./ExpenseForm.css";
 
 //* 비용 데이터를 모으는 폼의 구조화해서 사용자들이 title을 입력하도록 할 겁니다
 export const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredAmount, setEnteredAmount] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
-
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
   const titleChangeHandler = (e) => {
-    // setEnteredTitle(e.target.value);
-
-    // setUserInput({
-    //   ...userInput,
-    //   enteredTitle: e.target.value,
-    // });
-
-    setUserInput((prevState) => {
-      return { ...prevState, enteredTitle: e.target.value };
-    });
+    setEnteredTitle(e.target.value);
   };
 
   const amountChangeHandler = (e) => {
-    // setEnteredAmount(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredAmount: e.target.value,
-    // });
-
-    setUserInput((prevState) => {
-      return { ...prevState, enteredAmount: e.target.value };
-    });
+    setEnteredAmount(e.target.value);
   };
 
   const dateChangeHandler = (e) => {
-    // setEnteredDate(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredDate: e.target.value,
-    // });
+    setEnteredDate(e.target.value);
+  };
 
-    setUserInput((prevState) => {
-      return { ...prevState, enteredDate: e.target.value };
-    });
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    // ExpenseData 객체를 생성 ➡️ 그리고 입력된 모든 데이터를 결합
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
