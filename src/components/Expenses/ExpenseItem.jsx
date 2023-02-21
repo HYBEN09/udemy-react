5479770d6bc29d3b1152dfcfff9d0c05ac6641d8
hyebeen byun <hyebeen2658@naver.com>
2023-02-22 00:54:25 +0900

    ExpenseItem component 수정

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
 import Card from "../UI/Card";
 import { ExpenseDate } from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 export function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-  };
+  const { title } = props;
 
   return (
     <Card className="expense-item">
@@ -17,9 +12,7 @@ export function ExpenseItem(props) {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button type="button" onClick={clickHandler}>
-        Change Title
-      </button>
+      <button type="button">Change Title</button>
     </Card>
   );
 }
