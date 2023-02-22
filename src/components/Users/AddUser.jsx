746a21aa13239cf49030cc4b46cb671eb3d38889
hyebeen byun <hyebeen2658@naver.com>
2023-02-23 07:42:37 +0900

    재사용 가능한 Card 컴포넌트 만들기

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,19 +1,24 @@
+import Card from "../UI/Card";
+
+import classes from "./AddUser.module.css";
+
 // 사용자가 이름과 나이을 입력할 수 있게 하는 창과 그 모든 것을 확인하는 버튼
-
-const addUserHandler = (e) => {
-  e.preventDefault();
-};
-
 export function AddUser() {
+  const addUserHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">UserName</label>
-      <input id="username" type="text" />
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">UserName</label>
+        <input id="username" type="text" />
 
-      <label htmlFor="age">Age (Years )</label>
-      <input id="age" type="Number" />
+        <label htmlFor="age">Age (Years )</label>
+        <input id="age" type="Number" />
 
-      <button type="submit">Add User</button>
-    </form>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 }
