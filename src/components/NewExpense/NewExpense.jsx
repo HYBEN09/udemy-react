33674aa59607d3af77a5f50dbe3d8bb311481ab1
hyebeen byun <hyebeen2658@naver.com>
2023-02-22 22:40:02 +0900

    폼 조건부 출력

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { ExpenseForm } from "./ExpenseForm";
 import "./NewExpense.css";
 
 // 사용자들이 그들의 비용 데이터를 입력할 수 있는 입력창을 렌더링하고 싶습니다
 export const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData, //title,amount,date
@@ -11,9 +14,25 @@ export const NewExpense = (props) => {
     props.onAddExpense(expenseData);
   };
 
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}> Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
