eab0939aeddf8e940a3d282d58dc20b23912ceaa
hyebeen byun <hyebeen2658@naver.com>
2023-02-22 22:53:53 +0900

    컴포넌트 분리 - ExpensesList

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import { ExpenseItem } from "./ExpenseItem";
 import "./Expenses.css";
 import { ExpensesFilter } from "./ExpensesFilter";
+import { ExpensesList } from "./ExpensesList";
 
 export function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -16,29 +16,13 @@ export function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  // filteredExpenses 배열이 공백이라면 아무 것도 렌더링하지 않음
-  // filteredExpenses에 맞는 목록이 있으면 보여 주기
-
-  let expensesContent = <p>No expense found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
