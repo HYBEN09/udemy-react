be0980576e59134d9358b295955f70b21451ed6b
hyebeen byun <hyebeen2658@naver.com>
2023-02-26 09:52:57 +0900

    useRef 사용

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
@@ -7,44 +7,36 @@ import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css";
 
 //* 사용자가 이름과 나이을 입력할 수 있게 하는 창과 그 모든 것을 확인하는 버튼
-export function AddUser(props) {
-  //모든 키보드의 입력 상태를 업데이트하고 사용자가 입력하는 것을 state 변수에 저장
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
-  const [error, setError] = useState("");
+export function AddUser({ onAddUser }) {
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  const [error, setError] = useState();
 
   const addUserHandler = (e) => {
     e.preventDefault();
 
-    //*유효성 검사
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age (non-empty values).",
       });
       return;
     }
-    //나이값은 숫자가 아닌 문자열로 반환이 되어 안전하게 +enteredAge하여 숫자형으로 바꿔줌.
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: "Invalid age",
         message: "Please enter a valid age (> 0).",
       });
       return;
     }
-    //Add User 버튼이 클릭될 때마다 그 입력된 데이터들을 App 컴포넌트로 내보냅니다
-    props.onAddUser(enteredUsername, enteredAge);
+    onAddUser(enteredName, enteredUserAge);
 
-    setEnteredUsername("");
-    setEnteredAge("");
-  };
-
-  const usernameChangeHandler = (e) => {
-    setEnteredUsername(e.target.value);
-  };
-
-  const ageChangeHandler = (e) => {
-    setEnteredAge(e.target.value);
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   // 오류 모달창이 생겼을 때 Backdrop , Okay 버튼을 클릭하면 에러모달이 사라집니다
@@ -65,20 +57,10 @@ export function AddUser(props) {
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">UserName</label>
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
+          <input id="username" type="text" ref={nameInputRef} />
 
           <label htmlFor="age">Age (Years )</label>
-          <input
-            id="age"
-            type="Number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="Number" ref={ageInputRef} />
 
           <Button type="submit">Add User</Button>
         </form>
