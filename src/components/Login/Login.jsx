9fad2d5a98cdd334c4ae4adb9dd452f48f6df834
hyebeen byun <hyebeen2658@naver.com>
2023-02-27 21:50:23 +0900

    learn. useReducer& useEffect#4

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -47,16 +47,38 @@ const Login = (props) => {
     isValid: null,
   });
 
+  //* useEffect -------------------------------------------------------------
+
+  // 별칭 할당
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("Checking form validity!");
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500);
+
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(identifier);
+    };
+
+    // 값만 변경되고 유효성은 변경되지 않으면이 이펙트는 다시 실행되지 않습니다.
+  }, [emailIsValid, passwordIsValid]);
+
+  //*---------------------------------------------------------------------------
+
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
-    setFormIsValid(emailState.value.includes("@") && passwordState.isValid);
+    // setFormIsValid(emailState.value.includes("@") && passwordState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({ type: "USER_INPUT", val: event.target.value });
 
-    setFormIsValid(emailState.isValid && passwordState.value.trim().length > 6);
+    // setFormIsValid(emailState.isValid && passwordState.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
