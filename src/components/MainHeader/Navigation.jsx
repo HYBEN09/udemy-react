2eb177f6b99b5c85e403613d85566de52a06ae56
hyebeen byun <hyebeen2658@naver.com>
2023-03-01 01:36:37 +0900

    useContext hook 사용 #5

--- src/components/MainHeader/Navigation.jsx
+++ src/components/MainHeader/Navigation.jsx
@@ -1,35 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../../Context/AuthContext";
 
 import classes from "./Navigation.module.css";
 //사용자가 인증되었는지 여부를 알고 싶다
-const Navigation = () => {
+const Navigation = ({ onLogout }) => {
+  const { isLoggedIn } = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {({ isLoggedIn, onLogout }) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {isLoggedIn && (
-                <li>
-                  <button onClick={onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li>
+            <button onClick={onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
