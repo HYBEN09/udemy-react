ef9dc1632e2bf6275c7dbbd2c16e0212a4df9aa6
hyebeen byun <hyebeen2658@naver.com>
2023-03-02 12:19:42 +0900

    learn. POST 요청 보내기 -firebase 이용

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
+import { AddMovie } from "../components/AddMovie";
 import { MovieList } from "../components/MovieList";
 import "./App.css";
 
@@ -16,7 +17,10 @@ function App() {
     setError(null);
 
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      // firebase
+      const response = await fetch(
+        "https://react-http-e9efe-default-rtdb.firebaseio.com/movies.json"
+      );
 
       if (!response.ok) {
         throw new Error("Something Went Wrong");
@@ -24,15 +28,20 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      //loadedMovies에 전달받은 데이터의 키와 값들의 조합에 대해 새로운 객체를 푸쉬
+      //response로 받은 중첩 객체를 타고 들어가게 됩니다.
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -45,6 +54,23 @@ function App() {
     handlerFetchMovie();
     // 함수가 외부 상태를 사용한다면 의도치 않은 버그가 발생위험 ➡️ useCallback 훅을 사용
   }, [handlerFetchMovie]);
+
+  //------------------------------------------------------
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-e9efe-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
+  }
 
   //-------------------------------------------------------
 
@@ -66,6 +92,9 @@ function App() {
 
   return (
     <>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={handlerFetchMovie}>Fetch Movies</button>
       </section>
